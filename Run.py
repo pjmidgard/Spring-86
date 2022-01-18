b9cb958aeb55a0a8b77e49d5cbb0d60c1a51ef0c
Java Software <pj.midgard@yahoo.com>
2022-01-18 18:30:02 +0000

    Add files via upload

--- Run.py
+++ Run.py
@@ -3,55 +3,55 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import os
- 
- 
- 
+
 def back(img_no):
  
     # We will have global variable to access these
     # variable and change whenever needed
-   
+    global label
     global button_run
-    
-    button_run = Button(root, text="run",
-                            command)
-    
+    label.grid_forget()
+ 
+    # for clearing the image for new image to pop up
+    label = Label(image=List_images[img_no - 1])
+    label.grid(row=1, column=0, columnspan=3)
+    button_run = Button(root, text="Run")
+  
     print(img_no)
  
-    # whenever the first image will be there we will
-    # have the back button disabled
-    
  
     
+ 
+    label.grid(row=1, column=0, columnspan=3)
+
     button_for.grid(row=5, column=2)
  
  
-# Calling the Tk (The initial constructor of tkinter)
+
 root = Tk()
  
-# We will make the title of our app as Image Viewer
+
 root.title("Image Viewer")
  
-# The geometry of the box which will be displayed
-# on the screen
+
 root.geometry("700x700")
  
-# Adding the images using the pillow module which
-# has a class ImageTk We can directly add the
-# photos in the tkinter folder or we have to
-# give a proper path for the images
+
 image_no_1 = ImageTk.PhotoImage(Image.open("smaple1.jfif"))
 
  
-# List of the images so that we traverse the list
+
 List_images = [image_no_1]
  
-button_run = Button(root, text="run",
+label = Label(image=image_no_1)
+ 
+label.grid(row=1, column=0, columnspan=3)
+ 
+button_run = Button(root, text="Run",
                         command=lambda: os.startfile('Spring-86.py'))
 
  
-# grid function is for placing the buttons in the frame
-
 button_run.grid(row=5, column=2)
 
+ 
 root.mainloop()
